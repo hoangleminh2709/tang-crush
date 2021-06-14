8fad736a27b06fc3d3457b61058ef8d1eaf7ff7f
hoangleminh2709 <85881055+hoangleminh2709@users.noreply.github.com>
2021-06-14 19:42:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'vk ju à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'vân anh à!',
+    introDesc: `t đẹp trai vl yêu tao đi`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    title: 'minh hoàng đẹp trai wa tr wa đất 😙',
+    desc: 'minh hoàng đẹp trai đcđ ',
+    btnYes: 't dep trai',
+    btnNo: 't k dep trai',
+    question:'t dep trai vl dung k em yeu',
+    btnReply: 'minh hoang dep trai',
+    reply: 'hoang le minh dep trai so 2 khong ai so 1',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'địt không.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
